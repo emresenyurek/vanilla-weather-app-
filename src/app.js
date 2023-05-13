c089f65d36317866b47345fbcf591e578fe68909
Emre <emre.snyurek@gmail.com>
2023-05-13 13:25:28 +0300

    fixed apiUrl

--- src/app.js
+++ src/app.js
@@ -1,3 +1,3 @@
 let apiKey = "2669277795e3d0732a427f3b95ae9bf2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiUrl}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
 console.log(apiUrl);
